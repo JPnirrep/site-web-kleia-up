e038540e0fd462a167c3c0a25e37ad54c2048465
JPnirrep <jpp180866@gmail.com>
2026-05-16 11:39:53 +0200

    chore: cleanup db dump script

--- php/db-dump.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/plain; charset=utf-8');
-$db = __DIR__ . '/../data/atelier-inscriptions.json';
-if (!file_exists($db)) { echo "Aucune entree."; exit; }
-$data = json_decode(file_get_contents($db), true);
-echo count($data) . " entree(s) :\n\n";
-foreach ($data as $i => $e) {
-    echo ($i+1) . ". {$e['prenom']} {$e['nom']} | {$e['email']} | RGPD: " . ($e['consent']?'oui':'non') . " | {$e['created_at']} | Brevo: " . ($e['brevo_synced']?'sync':'non') . "\n";
-}
